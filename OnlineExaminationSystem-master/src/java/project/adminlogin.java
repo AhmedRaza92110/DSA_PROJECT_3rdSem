e7e9c6c28bbe4ce4d5b16ca447f03637be6acc1a
Ahmed Raza Qadri <160393062+AhmedRaza92110@users.noreply.github.com>
2025-01-03 21:12:06 +0500

    Update adminlogin.java

--- OnlineExaminationSystem-master/src/java/project/adminlogin.java
+++ OnlineExaminationSystem-master/src/java/project/adminlogin.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package project;
 
 import java.io.IOException;
@@ -21,7 +17,7 @@ import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author Abhay Shah
+ * 
  */
 @WebServlet(name = "adminlogin", urlPatterns = {"/adminlogin"})
 public class adminlogin extends HttpServlet {
